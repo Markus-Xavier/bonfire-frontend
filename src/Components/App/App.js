8a8e5408804e47b0feac8964a6c6636ad5dcd679
Markus Rossio <argitavis@gmail.com>
2022-03-09 22:22:09 -0600

    Add login
    Created login button componenet utilizing auth0 loginWithRedirect hook to redirect user on button click

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LoginButton from '../LoginButton/LoginButton';
 
 function App() {
   return (
     <div>
       <h1>Dog Boy</h1>
+      <LoginButton />
     </div>
   );
 }
