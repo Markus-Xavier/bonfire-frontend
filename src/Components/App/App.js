eb6de941a56b880d7de8973130b0b9f336837757
Markus Rossio <argitavis@gmail.com>
2022-03-10 04:13:47 -0600

    Inital socket io implimentation
    Installed socket io into project and created a socket connection request to backend

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import LoginButton from '../LoginButton/LoginButton';
+import { io } from 'socket.io-client';
 
 function App() {
+  const socket = io('http://localhost:4000');
+
   return (
     <div>
       <h1>Dog Boy</h1>
