43c9e1feb5c18e7124eeec5deb25a17059a2bbcb
Markus Rossio <argitavis@gmail.com>
2022-03-09 22:09:06 -0600

    Configure auth0 provider component
    Added configuration settings for auth0Provider to index js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './Services/WebVitals/reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+  domain="dev-c-6l4ag3.us.auth0.com"
+  clientId="GBlIPoi3KEZTi2QwSMR9H2pCvgtKY6VE"
+  redirectUri={window.location.origin}
+>
+  <App />
+</Auth0Provider>,
   document.getElementById('root')
 );
 
